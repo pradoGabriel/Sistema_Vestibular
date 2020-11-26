a24f9e011e6bb0ef2002697a5e14be4ede3b5fb3
pradoGabriel <gabriel.silva441@fatec.sp.gov.br>
2020-11-26 00:00:35 -0300

    Correções alexandre

--- Sistema_Vestibular/Sistema_Vestibular/src/View/AnaliseCandidatoView.java
+++ Sistema_Vestibular/Sistema_Vestibular/src/View/AnaliseCandidatoView.java
@@ -18,20 +18,23 @@ public class AnaliseCandidatoView {
 	    ListaSituacaoController listaSituacao = new ListaSituacaoController();
 	    listaSituacao = mp.lerSituacao();
 	    CandidatoModel[] aux = new CandidatoModel[listaCandidatos.tamanho()];
-	    aux=listaCandidatos.toVet();
+	    aux = percorreAvaliados(listaCandidatos, listaSituacao);
+	    
 	    
 	    System.out.println("***********************************************************************\n"
 	            + "Lista de Candidatos a serem analisados.!!\n"
 	            + "***********************************************************************");
-	  	   
-	    String mostrar = listaCandidatos.percorre();
-	    System.out.println(mostrar);
+	  	       
+	    for(int i=0;i<aux.length;i++){
+	    	if(aux[i]!=null)
+	    	System.out.println(i+1+"- "+aux[i]);
+	    }
 	    
 	    System.out.print("Escolha o candidato a ser analisado: ");
 	    int opt = ler.nextInt()-1;
 	 	menuAnalise(aux[opt]);
-		
 	}
+	
 
 	public void menuAnalise(CandidatoModel candidato) throws IOException{
 		ManipuladorArquivoController mp = new ManipuladorArquivoController();   
@@ -43,13 +46,37 @@ public class AnaliseCandidatoView {
 	    
 	    System.out.println(candidato.toString());
 	    System.out.println("*********************************************************************");
-	    System.out.println("Opï¿½ï¿½es:\n1 - Aprovado\n2 - Reprovado");
+	    System.out.println("Opções:\n1 - Aprovado\n2 - Reprovado");
 	    int opt = ler.nextInt();
+	    ler.nextLine();
 	    System.out.println("Detalhe o motivo");
-	    String detalhes = ler.next();
+	    String detalhes = ler.nextLine();
 	    String curso  = "curso";
 	    String linha = candidato.getNome()+";"+candidato.getCpf()+";"+opt+";"+curso+";"+detalhes;    
 	    mp.gravar(linha, "situacao.txt");
-	    
 	}
+	
+	private CandidatoModel[] percorreAvaliados(ListaCandidatosController listaCandidatos, ListaSituacaoController listaSituacao) {
+		CandidatoModel[] aux = new CandidatoModel[listaCandidatos.tamanho()];
+	    aux=listaCandidatos.toVet();
+	    SituacaoModel[] aux2 = new SituacaoModel[listaSituacao.tamanho()];
+	    aux2=listaSituacao.toVet();
+	    CandidatoModel[] candidatos = new CandidatoModel[listaCandidatos.tamanho()];
+	    boolean valido=true;
+	    int contador=1;
+	    for(int i=0;i<aux.length;i++){
+	    	for(int j=0;j<aux2.length;j++){
+	    		if(aux[i].getCpf().equals(aux2[j].getCpf())){
+	    			valido=false;
+	    		}
+	    	}
+	    	if(valido==true){
+	    		candidatos[contador-1] = aux[i];
+	    		contador++;
+	    	}
+	    	valido = true;
+	    }
+	    return candidatos;
+	}
+	
 }
